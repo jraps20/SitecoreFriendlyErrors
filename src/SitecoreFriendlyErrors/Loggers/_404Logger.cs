8d3a04ffbac1522cb022f113cff5da059027f5e8
John Rappel <jrappel@geekhive.com>
2017-11-28 12:06:12 -0600

    Resolved logger namespace issue.

--- src/SitecoreFriendlyErrors/Loggers/_404Logger.cs
+++ src/SitecoreFriendlyErrors/Loggers/_404Logger.cs
@@ -5,6 +5,6 @@ namespace SitecoreFriendlyErrors.Loggers
 {
     public static class _404Logger
     {
-        public static ILog Log => LogManager.GetLogger("CustomErrors._404Logger") ?? LoggerFactory.GetLogger(typeof(_404Logger));
+        public static ILog Log => LogManager.GetLogger("SitecoreFriendlyErrors.Loggers._404Logger") ?? LoggerFactory.GetLogger(typeof(_404Logger));
     }
 }
