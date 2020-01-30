0b6f3f481de6201355f870ba987c7b63ad21c2ee
John Rappel <john_rappel@epam.com>
2020-01-30 10:17:07 -0600

    Modified to work with Sitecore 9.x.

--- src/SitecoreFriendlyErrors/Pipelines/HttpRequestEnd/Set404StatusCode.cs
+++ src/SitecoreFriendlyErrors/Pipelines/HttpRequestEnd/Set404StatusCode.cs
@@ -11,15 +11,17 @@ namespace SitecoreFriendlyErrors.Pipelines.HttpRequestEnd
     {
         protected override void Execute(HttpRequestArgs args)
         {
+            var httpContext = args.HttpContext;
+
             // retain 500 response if previously set
-            if (HttpContext.Current.Response.StatusCode >= 500 || args.Context.Request.RawUrl == "/" || Sitecore.Context.Site == null)
+            if (HttpContext.Current.Response.StatusCode >= 500 || httpContext.Request.RawUrl == "/" || Sitecore.Context.Site == null)
                 return;
 
             // return if request does not end with value set in ItemNotFoundUrl, i.e. successful page
-            if (!args.Context.Request.Url.LocalPath.EndsWith(Settings.ItemNotFoundUrl, StringComparison.InvariantCultureIgnoreCase))
+            if (!httpContext.Request.Url.LocalPath.EndsWith(Settings.ItemNotFoundUrl, StringComparison.InvariantCultureIgnoreCase))
                 return;
 
-            _404Logger.Log.Warn($"Page Not Found on {Sitecore.Context.Site.Name}: {args.Context.Request.RawUrl}, current status: {HttpContext.Current.Response.StatusCode}");
+            _404Logger.Log.Warn($"Page Not Found on {Sitecore.Context.Site.Name}: {httpContext.Request.RawUrl}, current status: {HttpContext.Current.Response.StatusCode}");
             HttpContext.Current.Response.TrySkipIisCustomErrors = true;
             HttpContext.Current.Response.StatusCode = (int)HttpStatusCode.NotFound;
             HttpContext.Current.Response.StatusDescription = "Page not found";
